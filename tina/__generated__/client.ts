a1710d528159f434fa53d5b126e15ac9f1f0942c
shixinyu <stone.sxy@gmail.com>
2026-04-14 00:20:48 +0800

    Remove TinaCMS integration

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4002/graphql', token: '2e6364a9c3fc8ab19ffc5dd9790f207fa1d91a76', queries,  });
-export default client;
-  
